85bcdd9d3c99057948bfdc8d751fbb83a106d309
BauyrzhanAzimkhanov <azimkhanov4@gmail.com>
2026-06-19 10:26:10 +0500

    feat: added examples for iterators

--- week2/iterators.py
+++ week2/iterators.py
@@ -1,18 +1,37 @@
+import random
+
 class MyNumbers:
     def __iter__(self):
-        self.a = 1
+        self.number = 1
         return self
 
     def __next__(self):
-        if self.a <= 20:
-            x = self.a # x - reference for a
-            self.a += 1
+        if self.number <= 20:
+            x = self.number # x - reference for number
+            self.number += 1
             return x
         else:
             raise StopIteration
 
-myclass = MyNumbers()
-myiter = iter(myclass)
+class randomBooleans():
+    def __iter__(self):
+        self.booleans = [True, False, None]
+        return self
+    def __next__(self):
+        answer = random.choice(self.booleans)
+        if (answer != None):
+            return answer
+        else:
+            raise StopIteration
 
-for x in myiter:
-    print(x)
+random_booleans = randomBooleans()
+random_booleans_iterator = iter(random_booleans)
+
+for i in random_booleans_iterator:
+    print(i)
+
+# myclass = MyNumbers()
+# myiter = iter(myclass)
+
+# for x in myiter:
+#     print(x)
