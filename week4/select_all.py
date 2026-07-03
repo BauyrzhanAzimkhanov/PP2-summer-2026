ea83077dc43f7e087781e5759e59d1513c8f5a85
BauyrzhanAzimkhanov <azimkhanov4@gmail.com>
2026-07-03 09:29:41 +0500

    feat: added UPDATE example

--- week4/select_all.py
+++ week4/select_all.py
@@ -12,11 +12,14 @@ connection_parameters = {
 
 table_name = "car_manufactures"
 
-sql_instruction = f"""SELECT ALL * FROM {table_name}"""
+sql_instruction = f"""SELECT ALL * FROM {table_name};"""
 
 with psycopg2.connect(**connection_parameters) as connection:
     with connection.cursor() as cursor:
         cursor.execute(sql_instruction)
         result = cursor.fetchall()
+        column_names = []
+        for i in cursor.description:
+            column_names.append(i[0])
     connection.commit()
-print(result)
+print(len(result), "rows in result", "\n",column_names, "\n", result)
---
+++ week4/update.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+connection_parameters = {
+    "host": "localhost",
+    "database": "car_manufactures",
+    "user": "pp2_admin",
+    "password": "pp2_admin",
+    "port": "5432"
+}
+
+sql_instruction_safe = """UPDATE car_manufactures SET name = %s, foundation_year = %s WHERE id = %s;"""
+
+values = ("Mercedes-Benz", 1926, 2)
+
+with psycopg2.connect(**connection_parameters) as connection:
+    with connection.cursor() as cursor:
+        cursor.execute(sql_instruction_safe, values)
+    connection.commit()
